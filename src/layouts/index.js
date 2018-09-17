86083003789941e013c80bc26054f657bdf2f4e1
Yin Shanyang <me@yinshanyang.com>
2018-09-17 17:36:44 +0800

    v3

---
+++ src/layouts/index.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+
+import 'ui-tachyons-light'
+
+const classes = {
+  header: 'pv3 ph2 mh2 mh4-ns',
+  company: 'f4 fw6',
+  country: 'dib pl2 f6 fw4 ttu',
+  html: 'h-100',
+  body: 'h-100 sans-serif text-normal-80 bg-background-100'
+}
+
+const Header = () => (
+  <header className={classes.header}>
+    <div className='pv3 ph2 bb b--foreground-20'>
+      <div className={classes.company}>
+        Swarm
+        <span className={classes.country}>
+          Singapore
+        </span>
+      </div>
+    </div>
+  </header>
+)
+
+const Head = () => (
+  <Helmet>
+    <title>Swarm — An Independent Data Visualization Studio</title>
+    <meta name='description' content='An independent data visualization studio based in Singapore. We build tools that allow us to better think about and understand the world around us.' />
+    <meta name='keywords' content='swarm, data visualisation, data visualization, singapore, dashboard, cartography' />
+    <meta name='format-detection' content='telephone=no' />
+    <html className='h-100' />
+    <body className={classes.body} />
+    <style>{`
+      @-webkit-keyframes fade {
+        0%   { opacity: 0; }
+        100% { opacity: 1; }
+      }
+      @-moz-keyframes fade {
+        0%   { opacity: 0; }
+        100% { opacity: 1; }
+      }
+      @-o-keyframes fade {
+        0%   { opacity: 0; }
+        100% { opacity: 1; }
+      }
+      @keyframes fade {
+        0%   { opacity: 0; }
+        100% { opacity: 1; }
+      }
+
+      .animation-fade-0 {
+        -webkit-animation: fade 1s 0s forwards cubic-bezier(0.6, 0.2, 0.1, 1);
+      	-moz-animation:    fade 1s 0s forwards cubic-bezier(0.6, 0.2, 0.1, 1);
+      	-o-animation:      fade 1s 0s forwards cubic-bezier(0.6, 0.2, 0.1, 1);
+      	animation:         fade 1s 0s forwards cubic-bezier(0.6, 0.2, 0.1, 1);
+      }
+      .animation-fade-1 {
+        -webkit-animation: fade 1s 0.5s forwards cubic-bezier(0.6, 0.2, 0.1, 1);
+      	-moz-animation:    fade 1s 0.5s forwards cubic-bezier(0.6, 0.2, 0.1, 1);
+      	-o-animation:      fade 1s 0.5s forwards cubic-bezier(0.6, 0.2, 0.1, 1);
+      	animation:         fade 1s 0.5s forwards cubic-bezier(0.6, 0.2, 0.1, 1);
+      }
+
+    `}</style>
+  </Helmet>
+)
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Head />
+    <Header />
+    {children()}
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
---
+++ src/pages/404.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const classes = {
+  section: 'pv4 ph5 bb b--foreground-20',
+  lastSection: 'pv4 ph5',
+  subtitle: 'f6 ttu tracked',
+  blurb: 'f4 lh-copy measure',
+  body: 'f5 lh-copy measure',
+  link: 'text-normal no-underline bb b--foreground-20 dim'
+}
+
+export default () => (
+  <div>
+    <section className={classes.lastSection}>
+      <h3 className={classes.subtitle}>
+        How the fuck?
+      </h3>
+      <p className={classes.blurb}>
+        How the fuck did you even get here?
+      </p>
+      <p className={classes.body}>
+        I mean… it’s a single page site yos.
+      </p>
+    </section>
+  </div>
+)
